fc7d9d9d1961dc8f22e5e91dd0d7d2f0b36d8c4a
bohdankniazev <bknyazev@ukr.net>
2019-09-10 17:31:41 +0300

    Update PacmanDraw.cs

--- PacmanDraw.cs
+++ PacmanDraw.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Pacman.PacmanDraw
-// Assembly: WindowsGame1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 934DD255-E814-463C-81B7-5BC45736D2C2
-// Assembly location: C:\Users\bohda\Desktop\Pacman\Pacman.exe
+
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;

c1e71ac6207a1e3225e8a18b13620f0e4ab095bb
bohdankniazev <bknyazev@ukr.net>
2019-09-10 17:28:11 +0300

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -1,8 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Pacman.GameState
-// Assembly: WindowsGame1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 934DD255-E814-463C-81B7-5BC45736D2C2
-// Assembly location: C:\Users\bohda\Desktop\Pacman\Pacman.exe
 
 namespace Pacman
 {
